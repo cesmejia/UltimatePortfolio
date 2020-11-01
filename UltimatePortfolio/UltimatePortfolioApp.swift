4554009e575f0a90b1ce0230859ed0a2372e9f2c
Ces <cesmejia@gruposeara.com>
2020-11-01 08:35:50 -0600

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Ces Mejia on 01/11/20.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
